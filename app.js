4cc8918dad9fbf4742604f16743d66682407a644
Sothink <rebenkov2002@mail.ru>
2022-08-26 07:54:06 +0300

    feat: add function start

---
+++ app.js
@@ -0,0 +1,447 @@
+const Az = document.getElementById('Az');
+const Ct = document.getElementById('Ct');
+const So = document.getElementById('So');
+const Sh = document.getElementById('Sh');
+const complexitys = document.querySelector('.complexitys');
+
+
+
+
+
+Az.addEventListener('click', () => {
+    complexitys.classList.add('flex');
+    Az.classList.add('red')
+})
+
+Ct.addEventListener('click', () => {
+    complexitys.classList.add('flex');
+    Ct.classList.add('red')
+})
+
+So.addEventListener('click', () => {
+    complexitys.classList.add('flex');
+    So.classList.add('red')
+})
+
+Sh.addEventListener('click', () => {
+    complexitys.classList.add('flex');
+    Sh.classList.add('red')
+})
+
+
+
+
+
+
+
+
+
+let blue1 = document.createElement("img");
+let blue2 = document.createElement("img");
+let blue3 = document.createElement("img");
+let blue4 = document.createElement("img");
+let blue5 = document.createElement("img");
+let blue6 = document.createElement("img");
+let blue7 = document.createElement("img");
+let blue8 = document.createElement("img");
+let blue9 = document.createElement("img");
+let blue10 = document.createElement("img");
+let blue11 = document.createElement("img");
+let blue12 = document.createElement("img");
+
+
+
+let brown1 = document.createElement("img");
+let brown2 = document.createElement("img");
+let brown3 = document.createElement("img");
+let brown4 = document.createElement("img");
+let brown5 = document.createElement("img");
+let brown6 = document.createElement("img");
+let brown7 = document.createElement("img");
+let brown8 = document.createElement("img");
+let brown9 = document.createElement("img");
+let brown10 = document.createElement("img");
+let brown11 = document.createElement("img");
+let brown12 = document.createElement("img");
+let brown13 = document.createElement("img");
+let brown14 = document.createElement("img");
+let brown15 = document.createElement("img");
+let brown16 = document.createElement("img");
+let brown17 = document.createElement("img");
+let brown18 = document.createElement("img");
+let brown19 = document.createElement("img");
+let brown20 = document.createElement("img");
+let brown21 = document.createElement("img");
+
+
+let green1 = document.createElement("img");
+let green2 = document.createElement("img");
+let green3 = document.createElement("img");
+let green4 = document.createElement("img");
+let green5 = document.createElement("img");
+let green6 = document.createElement("img");
+let green7 = document.createElement("img");
+let green8 = document.createElement("img");
+let green9 = document.createElement("img");
+let green10 = document.createElement("img");
+let green11 = document.createElement("img");
+let green12 = document.createElement("img");
+let green13 = document.createElement("img");
+let green14 = document.createElement("img");
+let green15 = document.createElement("img");
+let green16 = document.createElement("img");
+let green17 = document.createElement("img");
+let green18 = document.createElement("img");
+
+
+
+blue1.src = "./assets/MythicCards/blue/blue1.png";
+blue2.src = "./assets/MythicCards/blue/blue2.png";
+blue3.src = "./assets/MythicCards/blue/blue3.png";
+blue4.src = "./assets/MythicCards/blue/blue4.png";
+blue5.src = "./assets/MythicCards/blue/blue5.png";
+blue6.src = "./assets/MythicCards/blue/blue6.png";
+blue7.src = "./assets/MythicCards/blue/blue7.png";
+blue8.src = "./assets/MythicCards/blue/blue8.png";
+blue9.src = "./assets/MythicCards/blue/blue9.png";
+blue10.src = "./assets/MythicCards/blue/blue10.png";
+blue11.src = "./assets/MythicCards/blue/blue11.png";
+blue12.src = "./assets/MythicCards/blue/blue12.png";
+
+
+brown1.src = "./assets/MythicCards/brown/brown1.png";
+brown2.src = "./assets/MythicCards/brown/brown2.png";
+brown3.src = "./assets/MythicCards/brown/brown3.png";
+brown4.src = "./assets/MythicCards/brown/brown4.png";
+brown5.src = "./assets/MythicCards/brown/brown5.png";
+brown6.src = "./assets/MythicCards/brown/brown6.png";
+brown7.src = "./assets/MythicCards/brown/brown7.png";
+brown8.src = "./assets/MythicCards/brown/brown8.png";
+brown9.src = "./assets/MythicCards/brown/brown9.png";
+brown10.src = "./assets/MythicCards/brown/brown10.png";
+brown11.src = "./assets/MythicCards/brown/brown11.png";
+brown12.src = "./assets/MythicCards/brown/brown12.png";
+brown13.src = "./assets/MythicCards/brown/brown13.png";
+brown14.src = "./assets/MythicCards/brown/brown14.png";
+brown15.src = "./assets/MythicCards/brown/brown15.png";
+brown16.src = "./assets/MythicCards/brown/brown16.png";
+brown17.src = "./assets/MythicCards/brown/brown17.png";
+brown18.src = "./assets/MythicCards/brown/brown18.png";
+brown19.src = "./assets/MythicCards/brown/brown19.png";
+brown20.src = "./assets/MythicCards/brown/brown20.png";
+brown21.src = "./assets/MythicCards/brown/brown21.png";
+
+
+green1.src = "./assets/MythicCards/green/green1.png";
+green2.src = "./assets/MythicCards/green/green2.png";
+green3.src = "./assets/MythicCards/green/green3.png";
+green4.src = "./assets/MythicCards/green/green4.png";
+green5.src = "./assets/MythicCards/green/green5.png";
+green6.src = "./assets/MythicCards/green/green6.png";
+green7.src = "./assets/MythicCards/green/green7.png";
+green8.src = "./assets/MythicCards/green/green8.png";
+green9.src = "./assets/MythicCards/green/green9.png";
+green10.src = "./assets/MythicCards/green/green10.png";
+green11.src = "./assets/MythicCards/green/green11.png";
+green12.src = "./assets/MythicCards/green/green12.png";
+green13.src = "./assets/MythicCards/green/green13.png";
+green14.src = "./assets/MythicCards/green/green14.png";
+green15.src = "./assets/MythicCards/green/green15.png";
+green16.src = "./assets/MythicCards/green/green16.png";
+green17.src = "./assets/MythicCards/green/green17.png";
+green18.src = "./assets/MythicCards/green/green18.png";
+
+let brownss = [
+    brown1,
+    brown2,
+    brown3,
+    brown4,
+    brown5,
+    brown6,
+    brown7,
+    brown8,
+    brown9,
+    brown10,
+    brown11,
+    brown12,
+    brown13,
+    brown14,
+    brown15,
+    brown16,
+    brown17,
+    brown21,
+    brown18,
+    brown19,
+    brown20,
+    brown21,
+]
+
+
+
+
+
+// ----------------BROWN-----------
+
+
+let brownEasy = [
+    brown11,
+    brown12,
+    brown13,
+    brown14,
+    brown21,
+]
+
+let brownNorm = [
+    brown1,
+    brown2,
+    brown3,
+    brown4,
+    brown5,
+    brown15,
+    brown16,
+    brown17,
+    brown21,
+    brown18,
+    brown19,
+    brown20,
+]
+
+let brownHard = [
+    brown6,
+    brown7,
+    brown8,
+    brown9,
+    brown10,
+]
+
+
+
+// ---------------BLUE-----------------
+
+let blueEasy = [
+    blue3,
+    blue4,
+    blue5,
+    blue10,
+]
+
+let blueNorm = [
+    blue7,
+    blue9,
+    blue11,
+    blue12,
+]
+
+let blueHard = [
+    blue1,
+    blue2,
+    blue6,
+    blue8,
+]
+
+
+// ----------------GREEN---------------------
+
+let greenEasy = [
+    green1,
+    green12,
+    green16,
+    green17,
+    green18,
+]
+
+let greenNorm = [
+    green7,
+    green8,
+    green9,
+    green10,
+    green11,
+    green13,
+    green14,
+    green15,
+]
+
+let greenHard = [
+    green2,
+    green3,
+    green4,
+    green5,
+    green6,
+]
+
+let azathothEasyDeck = [];
+
+let greenAz = 5;
+let brownAz = 9;
+let blueAz = 2;
+
+let greenCt = 4;
+let brownCt = 9;
+let blueCt = 2;
+
+let greenSo = 5;
+let brownSo = 9;
+let blueSo = 2;
+
+let greenSh = 6;
+let brownSh = 8;
+let blueSh = 2;
+
+
+
+
+// ---------------- VERY EASY ------------------------------------
+
+
+
+const veryEasyMixAz = () => {
+    for (let i = 0; i < greenAz; i++) {
+        azathothEasyDeck.push(greenEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < brownAz; i++) {
+        if (brownEasy.length === 0) {
+            azathothEasyDeck.push(brownNorm.splice(0, 1)[0])
+        } else {
+            azathothEasyDeck.push(brownEasy.splice(0, 1)[0])
+        }
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < blueAz; i++) {
+        azathothEasyDeck.push(blueEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+}
+
+// veryEasyMixAz();
+
+
+
+
+
+
+
+const veryEasyMixCt = () => {
+    for (let i = 0; i < greenCt; i++) {
+        azathothEasyDeck.push(greenEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < brownCt; i++) {
+        if (brownEasy.length === 0) {
+            azathothEasyDeck.push(brownNorm.splice(0, 1)[0])
+        } else {
+            azathothEasyDeck.push(brownEasy.splice(0, 1)[0])
+        }
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < blueCt; i++) {
+        azathothEasyDeck.push(blueEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+}
+
+// veryEasyMixCt();
+
+
+
+
+
+
+
+const veryEasyMixSo = () => {
+    for (let i = 0; i < greenSo; i++) {
+        azathothEasyDeck.push(greenEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < brownSo; i++) {
+        if (brownEasy.length === 0) {
+            azathothEasyDeck.push(brownNorm.splice(0, 1)[0])
+        } else {
+            azathothEasyDeck.push(brownEasy.splice(0, 1)[0])
+        }
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < blueSo; i++) {
+        azathothEasyDeck.push(blueEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+}
+
+// veryEasyMixSo();
+
+
+
+
+
+
+const veryEasyMixSh = () => {
+    for (let i = 0; i < greenSh; i++) {
+        if (greenEasy.length === 0) {
+            azathothEasyDeck.push(greenNorm.splice(0, 1)[0])
+        } else {
+            azathothEasyDeck.push(greenEasy.splice(0, 1)[0])
+        }
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < brownSh; i++) {
+        if (brownEasy.length === 0) {
+            azathothEasyDeck.push(brownNorm.splice(0, 1)[0])
+        } else {
+            azathothEasyDeck.push(brownEasy.splice(0, 1)[0])
+        }
+        // console.log('tgtg');
+    };
+    for (let i = 0; i < blueSh; i++) {
+        azathothEasyDeck.push(blueEasy.splice(0, 1)[0])
+        // console.log('tgtg');
+    };
+
+}
+
+veryEasyMixSh();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function arrayRandElement(arr) {
+    var rand = Math.floor(Math.random() * arr.length);
+    return arr[rand];
+}
+
+
+
+
+
+
+
+
+
+
+const openCard = document.querySelector('.open__card');
+openCard.appendChild(arrayRandElement(azathothEasyDeck));
+
+console.log(brownss);
+console.log(azathothEasyDeck);
+
+// console.log(openCard.childNodes[1])
+let fChild = openCard.childNodes[1];
+fChild.classList.add('card');
+// console.log(cards[2]);
+
+
--- assets/MythicCards/brown/index.js
+++ assets/MythicCards/brown/index.js
@@ -1,47 +1,100 @@
-import brown1 from './brown1.png';
-import brown2 from './brown2.png';
-import brown3 from './brown3.png';
-import brown4 from './brown4.png';
-import brown5 from './brown5.png';
-import brown6 from './brown6.png';
-import brown7 from './brown7.png';
-import brown8 from './brown8.png';
-import brown9 from './brown9.png';
-import brown10 from './brown10.png';
-import brown11 from './brown11.png';
-import brown12 from './brown12.png';
-import brown13 from './brown13.png';
-import brown14 from './brown14.png';
-import brown15 from './brown15.png';
-import brown16 from './brown16.png';
-import brown17 from './brown17.png';
-import brown18 from './brown18.png';
-import brown19 from './brown19.png';
-import brown20 from './brown20.png';
-import brown21 from './brown21.png';
+// import brown1 from './brown1.png';
+// import brown2 from './brown2.png';
+// import brown3 from './brown3.png';
+// import brown4 from './brown4.png';
+// import brown5 from './brown5.png';
+// import brown6 from './brown6.png';
+// import brown7 from './brown7.png';
+// import brown8 from './brown8.png';
+// import brown9 from './brown9.png';
+// import brown10 from './brown10.png';
+// import brown11 from './brown11.png';
+// import brown12 from './brown12.png';
+// import brown13 from './brown13.png';
+// import brown14 from './brown14.png';
+// import brown15 from './brown15.png';
+// import brown16 from './brown16.png';
+// import brown17 from './brown17.png';
+// import brown18 from './brown18.png';
+// import brown19 from './brown19.png';
+// import brown20 from './brown20.png';
+// import brown21 from './brown21.png';
+// const brown1 = new Image("./brown1.png");
+// const brown2 = new Image("./brown2.png");
+// const brown3 = new Image("./brown3.png");
+// const brown4 = new Image("./brown4.png");
+// const brown5 = new Image("./brown5.png");
+// const brown6 = new Image("./brown6.png");
+// const brown7 = new Image("./brown7.png");
+// const brown8 = new Image("./brown8.png");
+// const brown9 = new Image("./brown9.png");
+// const brown10 = new Image("./brown10.png");
+// const brown11 = new Image("./brown11.png");
+// const brown12 = new Image("./brown12.png");
+// const brown13 = new Image("./brown13.png");
+// const brown14 = new Image("./brown14.png");
+// const brown15 = new Image("./brown15.png");
+// const brown16 = new Image("./brown16.png");
+// const brown17 = new Image("./brown17.png");
+// const brown18 = new Image("./brown18.png");
+// const brown19 = new Image("./brown19.png");
+// const brown20 = new Image("./brown20.png");
+// const brown21 = new Image("./brown21.png");
 
-const cards = {
-  brown1,
-  brown2,
-  brown3,
-  brown4,
-  brown5,
-  brown6,
-  brown7,
-  brown8,
-  brown9,
-  brown10,
-  brown11,
-  brown12,
-  brown13,
-  brown14,
-  brown15,
-  brown16,
-  brown17,
-  brown18,
-  brown19,
-  brown20,
-  brown21,
-}
+// let brown1 = document.createElement("img");
+// let brown2 = document.createElement("img");
+// let brown3 = document.createElement("img");
+// let brown4 = document.createElement("img");
+// let brown5 = document.createElement("img");
+// let brown6 = document.createElement("img");
+// let brown7 = document.createElement("img");
+// let brown8 = document.createElement("img");
+// let brown9 = document.createElement("img");
+// let brown10 = document.createElement("img");
+// let brown11 = document.createElement("img");
+// let brown12 = document.createElement("img");
+// let brown13 = document.createElement("img");
+// let brown14 = document.createElement("img");
+// let brown15 = document.createElement("img");
+// let brown16 = document.createElement("img");
+// let brown17 = document.createElement("img");
+// let brown18 = document.createElement("img");
+// let brown19 = document.createElement("img");
+// let brown20 = document.createElement("img");
+// let brown21 = document.createElement("img");
+// brown1.src = "./brown1.png";
+// let imgbrown1 = brown1;
 
-export default cards
+// const openCard = document.querySelector('.open__card');
+// openCard.appendChild(img);
+// const cards = {
+//   brown1,
+//   brown2,
+//   brown3,
+//   brown4,
+//   brown5,
+//   brown6,
+//   brown7,
+//   brown8,
+//   brown9,
+//   brown10,
+//   brown11,
+//   brown12,
+//   brown13,
+//   brown14,
+//   brown15,
+//   brown16,
+//   brown17,
+//   brown18,
+//   brown19,
+//   brown20,
+//   brown21,
+// }
+
+// export default imgbrown1
+
+
+// const check = (n1, n2) => {
+//   return n1 + n2;
+// };
+// export default check
